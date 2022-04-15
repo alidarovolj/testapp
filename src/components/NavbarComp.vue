0ebe87927534296c08862ff41a9783bfa1a48c29
Olzhas <oljasalidarov@gmail.com>
2022-04-15 19:49:11 +0600

    ReadyProject

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-black py-5">
+        <div class="container mx-auto flex justify-between">
+            <img src="../assets/logo.png" alt="">
+            <div class="flex items-center text-white">
+                <i class="fa-regular fa-heart"></i>
+                <p class="ml-3">Избранное</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavbarComp',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar />
+    <div class="search">
+      <div class="flex items-center mx-auto w-1/2 py-20 relative">
+        <input class="w-full p-3" type="text" placeholder="Поиск">
+        <i class="fa fa-magnifying-glass"></i>
+      </div>
+    </div>
+    <div class="my-10 flex flex-wrap w-full justify-between">
+      <div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import navbar from '../components/NavbarComp.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    navbar
   }
 }
 </script>
